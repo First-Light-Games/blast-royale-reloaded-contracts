2c89bd58a4e0ec4e1a09d16eff8e7bc6c1bb2734
Gabriel Slomka <gabrielslomka@Gabriels-MacBook-Pro.local>
2024-04-29 10:10:31 +0100

    Fixes

--- src/Presale.sol
+++ src/Presale.sol
@@ -17,11 +17,11 @@ contract Presale is Ownable, Pausable, ReentrancyGuard {
     IERC20 public PaymentToken;
     IERC20 public NoobToken;
     bytes32 public MerkleRoot;
-    mapping(address => uint256) public Purchased;
+    mapping(address => uint32) public Purchased;
     uint256 public BuyPrice = 1;
     bool public USDCMutex;
 
-    event NoobPurchased(address user, uint256 amount, uint256 buyPrice);
+    event NoobPurchased(address user, uint32 amount, uint32 buyPrice);
 
     constructor(IERC20 _paymentToken, IERC20 _noobToken, bytes32 root) Ownable(msg.sender) {
         NoobToken = _noobToken;
@@ -29,12 +29,16 @@ contract Presale is Ownable, Pausable, ReentrancyGuard {
         PaymentToken = _paymentToken;
     }
 
-    function AmountPurchased() external view returns (uint256) {
+    function AmountPurchased() external view returns (uint32) {
         return Purchased[msg.sender];
     }
 
-    function Buy(bytes32[] calldata proof, uint256 amount, uint256 maxAmount) payable external nonReentrant {
-        uint256 finalPrice = BuyPrice * amount;
+    function GetBuyPrice(uint32 amount) external view returns (uint32) {
+        return BuyPrice;
+    }
+
+    function Buy(bytes32[] calldata proof, uint32 amount, uint32 maxAmount) payable external nonReentrant {
+        uint32 finalPrice = BuyPrice * amount;
         _verifyProof(proof, msg.sender, MerkleRoot, maxAmount);
         require(PaymentToken.balanceOf(msg.sender) >= finalPrice, "Not enough COIN to buy");
         _recordPurchase(amount, maxAmount);
@@ -42,13 +46,13 @@ contract Presale is Ownable, Pausable, ReentrancyGuard {
         emit NoobPurchased(msg.sender, amount, BuyPrice);
     }
 
-    function _recordPurchase(uint256 amount, uint256 maxAmount) private {
+    function _recordPurchase(uint32 amount, uint32 maxAmount) private {
         uint256 purchased = Purchased[msg.sender];
         require(purchased + amount <= maxAmount, "Insuficient allocation to purchase");
         Purchased[msg.sender] = purchased + amount;
     }
 
-    function _verifyProof(bytes32[] memory proof, address addr, bytes32 root, uint256 amount) private pure {   
+    function _verifyProof(bytes32[] memory proof, address addr, bytes32 root, uint32 amount) private pure {   
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(addr, amount))));
         require(MerkleProof.verify(proof, root, leaf), "Invalid proof");
     }
@@ -57,7 +61,7 @@ contract Presale is Ownable, Pausable, ReentrancyGuard {
         MerkleRoot = root;
     }
 
-    function UpdateBuyPrice(uint256 price) external onlyOwner {
+    function UpdateBuyPrice(uint32 price) external onlyOwner {
         BuyPrice = price;
     }
 
