db8240f8b7964fc10adb70053640d5d32e1bd122
0xcorby <56396423+0xCorbs@users.noreply.github.com>
2024-07-11 16:33:21 +0100

    add test and fix burnForRewards

--- src/BurnForRewards.sol
+++ src/BurnForRewards.sol
@@ -1,39 +1,39 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-import "./interfaces/IBlastEquipmentNFTBurnable.sol";
 import "./interfaces/IERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@magiceden-oss/erc721m/contracts/IERC721M.sol";
+import "./interfaces/IERC721Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Burnable.sol";
 
 /// @title Burn Blast Equipments and CS for vouchers
 /// @dev Blast Equipments ERC721 token
 /// @dev Craft Spice (CS) ERC20 token
-contract BurnBlastAssetsForRewards is AccessControl, Pausable, ReentrancyGuard {
-    
+contract BurnForRewards is AccessControl, Pausable, ReentrancyGuard {
+    enum Reward {
+        BlastBucks,
+        Noob
+    }
     struct BurntAssets {
-        uint256[] equipmentIds;
-        uint256 csAmount;
+        uint256[] equipmentIdsForBB;
+        uint256[] equipmentIdsForNoob;
+        uint256 csAmountForBB;
+        uint256 csAmountForNoob;
     }
 
     mapping(address => BurntAssets) public burntAssets;
     IERC20Burnable public craftSpice;
-    IBlastEquipmentNFT public blastEquipment;
+    IERC721 public blastEquipment;
 
     /// @notice Event Assets Burnt
-    event AssetsBurnt(
-        address burner,
-        uint256[] equipmentIds,
-        uint256 csAmount
-    );
+    event AssetsBurnt(address burner, uint256[] equipmentIds, uint256 csAmount);
 
     /// @dev Setup the two contracts it will interact with : ERC721 and ERC20
     constructor(address _blastEquipment, address _craftSpice) {
-         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        blastEquipment = IBlastEquipmentNFT(_blastEquipment);
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        blastEquipment = IERC721(_blastEquipment);
         craftSpice = IERC20Burnable(_craftSpice);
     }
 
@@ -42,15 +42,19 @@ contract BurnBlastAssetsForRewards is AccessControl, Pausable, ReentrancyGuard {
     /// @param _csAmount amount of CS
     function burnAssetsForRewards(
         uint256[] memory _tokenIds,
-        uint256 _csAmount
+        uint256 _csAmount,
+        Reward reward
     ) public whenNotPaused nonReentrant {
         address burner = _msgSender();
         BurntAssets storage userAssets = burntAssets[burner];
 
-
         if (_csAmount > 0) {
             craftSpice.burnFrom(burner, _csAmount);
-            userAssets.csAmount += _csAmount;
+            if (reward == Reward.BlastBucks) {
+                userAssets.csAmountForBB += _csAmount;
+            } else if (reward == Reward.Noob) {
+                userAssets.csAmountForNoob += _csAmount;
+            }
         }
 
         for (uint256 i = 0; i < _tokenIds.length; i++) {
@@ -59,16 +63,20 @@ contract BurnBlastAssetsForRewards is AccessControl, Pausable, ReentrancyGuard {
                 "only owner can burn"
             );
             blastEquipment.burn(_tokenIds[i]);
-            userAssets.equipmentIds.push(_tokenIds[i]);
+            if (reward == Reward.BlastBucks) {
+                userAssets.equipmentIdsForBB.push(_tokenIds[i]);
+            } else if (reward == Reward.Noob) {
+                userAssets.equipmentIdsForNoob.push(_tokenIds[i]);
+            }
         }
         emit AssetsBurnt(burner, _tokenIds, _csAmount);
     }
 
     function getBurntAssets(
         address _address
-    ) public view returns (uint256[] memory, uint256) {
+    ) public view returns (BurntAssets memory) {
         BurntAssets memory _burntAssets = burntAssets[_address];
-        return (_burntAssets.equipmentIds, _burntAssets.csAmount);
+        return (_burntAssets);
     }
 
     function pause(bool stop) external onlyRole(DEFAULT_ADMIN_ROLE) {
