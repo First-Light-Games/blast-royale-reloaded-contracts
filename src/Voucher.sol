fe5f9404886d143b47fedf92a8b6033b1756840a
0xcorby <56396423+0xCorbs@users.noreply.github.com>
2024-09-11 00:23:34 +0100

    web2 to web3 portal voucher

--- src/Voucher.sol
+++ src/Voucher.sol
@@ -10,10 +10,12 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 contract Voucher is EIP712 {
     string private constant SIGNING_DOMAIN = "FLG";
     string private constant SIGNATURE_VERSION = "1";
-    address public adminAddress;
+    address public signerAddress;
 
-    constructor(address _adminAddress) EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {
-        adminAddress = _adminAddress;
+    constructor(
+        address _signerAddress
+    ) EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {
+        signerAddress = _signerAddress;
     }
 
     /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain.
@@ -34,31 +36,40 @@ contract Voucher is EIP712 {
 
     /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
     /// @param voucher An NFTVoucher to hash.
-    function SignEIP712(NFTVoucher calldata voucher) public view returns (bytes32) {
-        return _hashTypedDataV4(
-            keccak256(
-                abi.encode(
-                    keccak256("NFTVoucher(bytes16 voucherId,bytes32[] data,address wallet,uint64 signatureType)"),
-                    voucher.voucherId,
-                    keccak256(abi.encodePacked(voucher.data)),
-                    voucher.wallet,
-                    voucher.signatureType
+    function SignEIP712(
+        NFTVoucher calldata voucher
+    ) public view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "NFTVoucher(bytes16 voucherId,bytes32[] data,address wallet,uint64 signatureType)"
+                        ),
+                        voucher.voucherId,
+                        keccak256(abi.encodePacked(voucher.data)),
+                        voucher.wallet,
+                        voucher.signatureType
+                    )
                 )
-            )
-        );
+            );
     }
 
     /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
     /// @dev Will revert if the signature is invalid.
     /// Does not verify that the signer is authorized to mint NFTs.
     /// @param voucher An NFTVoucher describing an unminted NFT.
-    function IsValidSignature(NFTVoucher calldata voucher) public view returns (bool) {
+    function IsValidSignature(
+        NFTVoucher calldata voucher
+    ) public view returns (bool) {
         bytes32 digest = SignEIP712(voucher);
-        return ECDSA.recover(digest, voucher.signature) == adminAddress;
+        return ECDSA.recover(digest, voucher.signature) == signerAddress;
     }
 
     /// Parses Uint256 from byte32s
-    function ParseUint256(bytes32[] memory data) public pure returns (uint256[] memory) {
+    function ParseUint256(
+        bytes32[] memory data
+    ) public pure returns (uint256[] memory) {
         uint256[] memory array = new uint256[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
             array[i] = uint256(data[i]);
