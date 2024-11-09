c7d2aa190183c731c375580ff1eea5dcd5a6680a
JCollinsX <nonfungiblejc@gmail.com>
2024-11-10 04:01:57 +0900

    fix: make totalRewardsLimit as private and pass as constructor arg

--- script/NoobFlexibleStaking.s.sol
+++ script/NoobFlexibleStaking.s.sol
@@ -8,12 +8,13 @@ import "../src/NoobToken.sol";
 contract NoobFlexibleStakingScript is Script {
     address public adminAddress = 0xd540AB459B33f1B45D948f3edFd1B4Bbd810fD6d;
     address public noobAddress = 0x7866fbB00a197d5abab0aB666F045C2caA879ffC;
+    uint256 public tgeStart = 1731499200; // 13/11 12:00 UTC
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        NoobFlexibleStaking flexibleStaking = new NoobFlexibleStaking( noobAddress, 36500, adminAddress);
+        NoobFlexibleStaking flexibleStaking = new NoobFlexibleStaking( noobAddress, 36500, adminAddress, tgeStart);
 
         vm.stopBroadcast();
     }
